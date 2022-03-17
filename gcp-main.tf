fae8afdc8065d4fd573ef8a19366e5810198cdfe
Lucas Barros <lucas.bsouza2@gmail.com>
2022-03-17 00:39:06 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -20,3 +20,17 @@ resource "google_compute_instance" "firstvm" {
     }
   }
 }
+
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-cloud-344401"
+  location_id = "us-central"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+
+  location = "us-central1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
--- providers.tf
+++ providers.tf
@@ -9,7 +9,12 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
+  project = "lab-devops-cloud-344401"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
 
+provider "google-beta" {
   project = "lab-devops-cloud-344401"
   region  = "us-west1"
   zone    = "us-west1-b"
