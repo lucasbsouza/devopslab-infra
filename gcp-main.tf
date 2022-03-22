cc71f830b01521657bdf3b7dcab910d80cc5e687
Lucas de Barros <lucas.bsouza2@gmail.com>
2022-03-22 00:20:02 -0300

    Teste 2

--- gcp-main.tf
+++ gcp-main.tf
@@ -21,10 +21,10 @@ resource "google_compute_instance" "firstvm" {
   }
 }
 
-resource "google_app_engine_application" "app" {
-  project     = "lab-devops-340709"
-  location_id = "us-central"
-}
+# resource "google_app_engine_application" "app" {
+#  project     = "lab-devops-340709"
+#  location_id = "us-central"
+# }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
